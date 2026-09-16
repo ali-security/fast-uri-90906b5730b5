719601f9adc3424281327517e4a890319e81d535
Seal <info@sealsecurity.io>
2026-09-16 21:13:16 +0000

    CVE-2026-84292

--- index.js
+++ index.js
@@ -440,7 +440,11 @@ function normalizeComparableURI (uri, opts) {
   }
 
   if (typeof uri === 'object') {
-    return serialize(uri, opts)
+    try {
+      return serialize(uri, opts)
+    } catch {
+      return undefined
+    }
   }
 }
 
